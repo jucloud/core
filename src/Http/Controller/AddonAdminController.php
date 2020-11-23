fcbebad4a65229dbcd8800b0277cf9887bba28ae
王凯 <327240570@qq.com>
2020-11-23 14:52:20 +0800

    增加插件控制器

---
+++ src/Http/Controller/AddonAdminController.php
@@ -0,0 +1,56 @@
+<?php
+namespace JuCloud\Core\Http\Controller;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+use Dcat\Admin\Support\Helper;
+use Dcat\Admin\Admin;
+
+class AddonAdminController extends Controller
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * 获取组件名称
+     * @Author    王凯
+     * @DateTime  2020-11-23
+     * @copyright [copyright]
+     * @license   [license]
+     * @version   [version]
+     * @return    [type]
+     */
+    public function getAddonName() {
+
+    	$uris = explode('/addons/', request()->url())[1];
+
+    	$addonName = ucwords(substr($uris, 0, strpos($uris, "/")));
+    	return $addonName;
+    }
+
+    /**
+     * 获取左侧菜单
+     * @Author    王凯
+     * @DateTime  2020-11-23
+     * @copyright [copyright]
+     * @license   [license]
+     * @version   [version]
+     * @return    [type]
+     */
+    public function getSidebarMenu() {
+        
+        $config = json_decode(app()->make(Filesystem::class)->get(base_path('addons/' . ucwords($this->getAddonName())) . '/config.json'), true);
+
+        admin_inject_section(Admin::SECTION['LEFT_SIDEBAR_MENU'], function () use ($config) {
+            $builder = Admin::menu();
+            $html = '';
+            foreach (Helper::buildNestedArray($config['menu']) as $item) {
+                $html .= view('admin::partials.menu', ['item' => $item, 'builder' => $builder])->render();
+            }
+            return $html;
+        });
+    }
+}
